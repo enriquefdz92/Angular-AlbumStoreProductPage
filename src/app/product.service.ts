948b8300df0d8824a53a772442648b0949729b44
Fernandez <Enrique.Fernandez@gulfstream.com>
2019-06-18 14:36:50 -0700

    album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operator/map';
+@Injectable()
+export class ProductService {
+  _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) {}
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).map((response) => response.json());
+  }
+}
